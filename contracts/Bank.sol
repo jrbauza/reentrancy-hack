d1a6abc9f1493a313c7d4dc6383ade746a63955c
Juan Ramis <ramisjuan1@gmail.com>
2022-11-05 16:04:02 +0100

    protected withdraw method to prevent it from being partially executed

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -5,18 +5,25 @@ import "hardhat/console.sol";
 contract Bank{
 
     mapping (address => uint) private balances;
+    bool private locked;
+
+    constructor(){
+        locked = false;
+    }
 
     function deposit() public payable {
         balances[msg.sender] = msg.value;
     }
 
-    function withdraw(uint amount) public {
+    function withdraw(uint amount) public{
+        require(!locked, "Reentrant not avaialable");
+        locked = true;
         require(balances[msg.sender] >= amount, "Your balance is insufficient.");
-        
         console.log("Bank balance: %s", address(this).balance);
         console.log("Amount to withdraw %s", amount);
         (bool success, ) = msg.sender.call{value:amount}("");
         balances[msg.sender] = balances[msg.sender] - amount;
         require(success, "Failed to withdraw eth");
+        locked = false;
     }
 }
