7ff3ce4388115fc84eec8f132eacef6b96e561e8
Juan Ramis <ramisjuan1@gmail.com>
2022-11-07 00:07:50 +0100

    lockable modifier, withdraw locked, user balance

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -6,17 +6,36 @@ contract Bank{
 
     mapping (address => uint) private balances;
 
-    function deposit() public payable {
-        balances[msg.sender] = msg.value;
+    bool private locked;
+
+    constructor(){
+        locked = false;
     }
 
-    function withdraw(uint amount) public {
-        require(balances[msg.sender] >= amount, "Your balance is insufficient.");
+    function deposit() public payable {
+        balances[msg.sender] = balances[msg.sender] + msg.value;
+    }
+
+    function withdraw(uint amount) public lockable{
+        require(amount <= balances[msg.sender], "Your balance is insufficient.");
         
         console.log("Bank balance: %s", address(this).balance);
         console.log("Amount to withdraw %s", amount);
-        (bool success, ) = msg.sender.call{value:amount}("");
+        (bool success, bytes memory data) = msg.sender.call{value:amount}("");
         balances[msg.sender] = balances[msg.sender] - amount;
-        require(success, "Failed to withdraw eth");
+        console.log("DATA: %s, SIZE: %s", string(data), data.length);
+        if (!success) {
+            console.log("Not success");
+            (string memory mes, string memory message) = abi.decode(data, (string, string));
+            console.log("D: %s, S: %s", message, mes);
+            require(success, message);
+        }
+    }
+
+    modifier lockable {
+        require(!locked, "Feature locked!");
+        locked = true;
+        _;
+        locked = false;
     }
 }
